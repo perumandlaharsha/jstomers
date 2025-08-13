9f3e070921255617c5d850eef7409c31a386aceb
perumandlaharsha <perumandlaharsha05@gmail.com>
2025-08-13 15:02:12 +0530

    assignment ope

--- operators/operators.js
+++ operators/operators.js
@@ -3,5 +3,22 @@ a +=30;
 console.log(a);
 
 let b=50;
-b *=2;
+b -=2;
 console.log(b);
+
+let c=100;
+c *=5;
+console.log(c);
+
+let d=30;
+d /=3;
+console.log(d)
+
+let e=10;
+e %=2;
+console.log(e);
+
+let f=3;
+f **=3;
+console.log(f);
+
