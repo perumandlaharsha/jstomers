34279e57ca41766cec4288fa7a75b124b9583fe0
perumandlaharsha <perumandlaharsha05@gmail.com>
2025-08-13 15:05:46 +0530

    arithmetic ope

--- operators/operators.js
+++ operators/operators.js
@@ -1,3 +1,28 @@
+let a1=200;
+let a2=300;
+a3=(a1+a2);
+console.log(a3)
+
+let b1=200;
+let b2=300;
+b3=(b1-b2);
+console.log(b3)
+
+let c1=200;
+let c2=300;
+c3=(c1*c2);
+console.log(c3)
+
+let d1=200;
+let d2=300;
+d3=(d1/d2);
+console.log(d3)
+
+
+
+
+
+
 let a=10;
 a +=30;
 console.log(a);
